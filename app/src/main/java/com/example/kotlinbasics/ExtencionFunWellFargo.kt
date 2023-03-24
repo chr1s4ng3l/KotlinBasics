9ac9d06f088c2c05d52dbefab9567bc1e87c4f08
Consultant <christopher.tamayocazarez@gmail.com>
2023-03-24 10:37:13 -0400

    LinkedList

--- app/src/main/java/com/example/kotlinbasics/ExtencionFunWellFargo.kt
+++ app/src/main/java/com/example/kotlinbasics/ExtencionFunWellFargo.kt
@@ -1,2 +1,34 @@
 package com.example.kotlinbasics
 
+fun main(){
+
+
+    val myArray = arrayOf(1,3,3,3)
+
+    println(myArray.toString())
+    println(myArray.toExt())
+
+}
+
+
+
+/**
+ * Returns the number of unique elements in this array.
+ *
+ * @return the number of unique elements in this array
+ */
+fun Array<Int>.toExt(): Int {
+    // Sort the array to group equal elements together
+    val array = this.sorted()
+    // Initialize the fold operation with a null previous element and a count of 0
+    val initial = Pair(null as Int?, 0)
+    // Use fold to count the number of unique elements
+    val reduced = array.fold(initial) { (prev, count), curr ->
+        // If the current element is equal to the previous element, don't increment the count
+        // Otherwise, increment the count by 1
+        Pair(curr, if (prev == curr) count else count + 1)
+    }
+    // Return the count of unique elements
+    return reduced.second
+}
+
