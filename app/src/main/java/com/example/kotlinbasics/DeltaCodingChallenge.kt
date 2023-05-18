a0d21cc3e2653db99e7cc5b388c903e947eadf80
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-05-18 15:03:18 -0400

    DeltaCodingChallenge.kt

--- app/src/main/java/com/example/kotlinbasics/DeltaCodingChallenge.kt
+++ app/src/main/java/com/example/kotlinbasics/DeltaCodingChallenge.kt
@@ -1,2 +1,22 @@
 package com.example.kotlinbasics
 
+
+fun main() {
+    val input = "aabbuewcdcd"
+
+    println(input.toRemoveDuplicateValues())
+}
+
+
+fun String.toRemoveDuplicateValues(): String {
+    val input = this.split("")
+    val charCountMap = input.groupingBy { it }.eachCount()
+
+    var a = ""
+    input.forEach {
+        if (charCountMap[it] == 1){
+            a += it
+        }
+    }
+    return a
+}
