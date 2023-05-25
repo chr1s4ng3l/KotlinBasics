3c9aacfdc990e6d8e46b23729cf46556dfe812d9
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-05-25 08:37:13 -0400

    TimeTo24

--- app/src/main/java/com/example/kotlinbasics/TimeTo24.kt
+++ app/src/main/java/com/example/kotlinbasics/TimeTo24.kt
@@ -1,2 +1,18 @@
 package com.example.kotlinbasics
 
+import android.annotation.SuppressLint
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+
+@SuppressLint("SimpleDateFormat")
+fun main() {
+
+
+    val parse = SimpleDateFormat("hh:mm:ss a")
+
+    val b = parse.parse("07:01:00 PM")
+
+    println(b)
+
+}
